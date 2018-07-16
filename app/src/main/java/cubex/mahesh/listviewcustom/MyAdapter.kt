a000e94520e0a83d71b5ee795b856e3448e62c1b
maheshthippala <mahesh.cubex@gmail.com>
2018-07-16 09:06:35 +0530

    new commit

--- app/src/main/java/cubex/mahesh/listviewcustom/MyAdapter.kt
+++ app/src/main/java/cubex/mahesh/listviewcustom/MyAdapter.kt
@@ -1,11 +1,16 @@
 package cubex.mahesh.listviewcustom
 
+import android.graphics.BitmapFactory
+import android.media.ThumbnailUtils
+import android.net.Uri
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import kotlinx.android.synthetic.main.indiview.view.*
 import java.io.File
 
-class MyAdapter : BaseAdapter()
+class MyAdapter : BaseAdapter
 {
     var files:Array<File>? = null
     var activity:MainActivity? = null
@@ -17,6 +22,22 @@ class MyAdapter : BaseAdapter()
 
     override fun getView(p0: Int, p1: View?, p2: ViewGroup?): View {
 
+   var inflater = LayoutInflater.from(activity)
+   var v = inflater.inflate(R.layout.indiview,null)
+
+       var file = files!![p0]
+       // v.iv1.setImageURI(Uri.fromFile(file))
+        var b = BitmapFactory.decodeFile(file.path)
+       var bmp = ThumbnailUtils.extractThumbnail(b,50,50)
+        v.iv1.setImageBitmap(bmp)
+        v.tv1.text = file.name
+        v.tv2.text = file.length().toString()
+        v.b1.setOnClickListener({
+            file.delete()
+            activity!!.readFiles()
+        })
+
+        return  v
     }
 
     override fun getItem(p0: Int): Any {
